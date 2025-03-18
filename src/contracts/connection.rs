5b61d1ad62c5b9ebd278336a231b6749f826ddba
Marcos Roque <devmarcosdanielr@gmail.com>
2025-03-18 00:54:00 -0300

    fix: db connection args name

--- src/contracts/connection.rs
+++ src/contracts/connection.rs
@@ -1,8 +1,9 @@
 use crate::common::error::DbError;
 
 pub trait DbConnection {
-    fn connect(connection_string: &str) -> Result<Self, DbError>
+    fn connect(connection_url: &str) -> Result<Self, DbError>
     where
         Self: Sized;
+
     fn close(&self) -> Result<(), DbError>;
 }
