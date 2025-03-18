8254ae4566a0092d612e53006da6a7c8af7af5f9
Marcos Roque <devmarcosdanielr@gmail.com>
2025-03-18 00:49:33 -0300

    refactor: create tests folder

--- src/modules/pgsql/connection.rs
+++ src/modules/pgsql/connection.rs
@@ -3,7 +3,7 @@ use postgres::{Client, NoTls};
 use crate::{common::error::DbError, contracts::connection::DbConnection};
 
 pub struct PostgresConnection {
-    client: Client,
+    pub client: Client,
 }
 
 impl DbConnection for PostgresConnection {
@@ -16,46 +16,5 @@ impl DbConnection for PostgresConnection {
 
     fn close(&self) -> Result<(), DbError> {
         Ok(())
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use std::{env, time::Duration};
-
-    use super::*;
-    use crate::common::error::DbError;
-
-    #[test]
-    fn test_connect_success() {
-        let connection_url = env::var("TEST_DATABASE_URL").expect("TEST_DATABASE_URL not set");
-        let connection_url_str: &str = connection_url.as_str();
-
-        let result = PostgresConnection::connect(connection_url_str);
-
-        match result {
-            Ok(mut conn) => {
-                let timeout = Duration::from_secs(5);
-                assert!(conn.client.is_valid(timeout).is_ok());
-            }
-            Err(err) => {
-                panic!("Connection failed: {}", err);
-            }
-        }
-    }
-
-    #[test]
-    fn test_connect_failure() {
-        let invalid_connection_string =
-            "postgres://wronguser:wrongpassword@invalidhost:5432/nonexistentdb";
-
-        let result = PostgresConnection::connect(invalid_connection_string);
-
-        match result {
-            Ok(_) => panic!("Connection should have failed"),
-            Err(err) => {
-                assert!(matches!(err, DbError::ConnectionFailed(_)));
-            }
-        }
     }
 }
